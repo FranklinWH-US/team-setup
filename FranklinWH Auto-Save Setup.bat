bebcb2db806befb2287f7e317bffbf34793763dd
Franklin Team <TI@FranklinWH.com>
2026-05-14 15:40:54 -0400

    Update .bat to pull from FranklinWH-US/team-setup

--- FranklinWH Auto-Save Setup.bat
+++ FranklinWH Auto-Save Setup.bat
@@ -1,4 +1,4 @@
 @echo off
 title FranklinWH Auto-Save Setup
 color 0A
-powershell -ExecutionPolicy Bypass -Command "irm 'https://gist.githubusercontent.com/CBullardFranklin/8d3039adb923785068d76d3138d9a5d3/raw/setup-autosave.ps1' | iex"
+powershell -ExecutionPolicy Bypass -Command "irm 'https://raw.githubusercontent.com/FranklinWH-US/team-setup/main/setup-autosave.ps1' | iex"
